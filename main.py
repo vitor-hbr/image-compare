7f7584c9697178e6df7625a0e28ed87719e10e17
Vitor Brasiliense <vitor.hbr@outlook.com>
2022-12-31 18:40:44 -0300

    feat: Added video hash compare

--- main.py
+++ main.py
@@ -3,32 +3,70 @@ import shutil
 import uuid
 from PIL import Image
 import imagehash
+import hashlib
 
-# Create a new folder to store the unique images
-folder_name = "unique_images"
-os.makedirs(folder_name, exist_ok=True)
+folder_name = "unique_files"
+unique_file_hashes = {}
 
-# Recursively iterate over all the subdirectories in the current directory
-for root, dirs, files in os.walk("."):
-    # Iterate over all the images in the current directory
-    for image_file in files:
-        # Check if the file is an image
-        if image_file.endswith(".jpg") or image_file.endswith(".png"):
-            # Open the image and compute its pHash
-            with Image.open(os.path.join(root, image_file)) as image:
-                image_hash = imagehash.phash(image)
+def HashImage(folder_name, file):
+    with Image.open(os.path.join(folder_name, file)) as image:
+        return imagehash.phash(image)
 
-            # Check if the image hash is unique
-            unique = True
-            for other_image_file in os.listdir(folder_name):
-                with Image.open(os.path.join(folder_name, other_image_file)) as other_image:
-                    other_image_hash = imagehash.phash(other_image)
-                if image_hash == other_image_hash:
-                    unique = False
-                    break
+def HashVideo(folder_name, file):
+    with open(os.path.join(folder_name, file), "rb") as video:
+        return hashlib.sha256(video.read()).hexdigest()
 
-            # If the image is unique, copy it to the new folder and rename it with the path to the original image and a UUID
+def InitializeUniqueHashesDict():
+    for file in os.listdir(folder_name):
+        print(file)
+        file_hash = False
+
+        if file.endswith(".jpg") or file.endswith(".png"):
+            file_hash = HashImage(folder_name, file)
+        elif file.endswith(".mp4") or file.endswith(".avi"):
+            file_hash = HashVideo(folder_name, file)
+
+        if file_hash:
+            unique_file_hashes[file_hash] = True
+
+def IsFileUnique(root, file_path, file_type = 'image'):
+    file_hash = False
+
+    if file_type == 'image':
+        file_hash = HashImage(root, file_path)
+    elif file_type == 'video': 
+        file_hash = HashVideo(root, file_path)
+
+    if file_hash in unique_file_hashes:
+        return False
+    else: 
+        unique_file_hashes[file_hash] = True
+        return True            
+           
+def SaveUniqueFileToFolder(root, folder_name, file_path):
+    new_file_path = os.path.join(folder_name, file_path)
+    shutil.copy(os.path.join(root, file_path), new_file_path)
+    file_name = file_path.rsplit('.', 1)[0]
+    file_ext = file_path.rsplit('.', 1)[1]
+    os.rename(new_file_path, os.path.join(folder_name, (((root + "-" + file_name + str(uuid.uuid4())).replace(os.sep,'-')).replace('.','')) + '.' + file_ext))
+
+
+def GenerateAllUniqueImages():
+    os.makedirs(folder_name, exist_ok=True)
+    InitializeUniqueHashesDict()
+    for root, dirs, files in os.walk("."):
+        if root == "./" + folder_name:
+            continue
+        for file in files:
+            unique = False
+
+            if file.endswith(".jpg") or file.endswith(".png"):
+                unique = IsFileUnique(root, file, 'image')
+
+            elif file.endswith(".mp4") or file.endswith(".avi"):
+                unique = IsFileUnique(root, file, 'video')
+
             if unique:
-                shutil.copy(os.path.join(root, image_file), folder_name)
-                new_image_file = os.path.join(folder_name, image_file)
-                os.rename(new_image_file, new_image_file + " - " + root + " - " + str(uuid.uuid4()))
+                SaveUniqueFileToFolder(root, folder_name, file)
+
+GenerateAllUniqueImages()
